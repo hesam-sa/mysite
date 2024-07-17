46669e0ef11b1425946f8673ca10340daa2b4624
hesam-sa <hesam.sadat@gmail.com>
2024-07-17 19:17:31 +0330

    complete homework
    doing homework according to my mentor advice

--- blog/views.py
+++ blog/views.py
@@ -4,36 +4,20 @@ from django.shortcuts import render,get_object_or_404
 
 from django.http import HttpResponse,JsonResponse
 from blog.models import Post
-import datetime
+from django.utils import timezone
 
 
 
 def blog_view(request):
-    post=Post.objects.filter(status=1)
+    now=timezone.now()
+    post1=Post.objects.filter(status=1)
+    post=post1.filter(published_date__lte=now)
     context = {'post':post}
     return render(request,'blog/blog-home.html',context)
 
 def blog_single(request,pid):
     post=get_object_or_404(Post,pk=pid)
-    context = {'post':post}
-    return render(request,'blog/blog-single.html',context)
-
-def test(request):
-    now=datetime.datetime.now()
-    post = Post.objects.filter(published_date__lte=now)
-    context = {'post':post,'now':now}
-    return render(request,'test.html',context)
-
-def test2(request,pid):
-    post= Post.objects.get(id=pid)
     post.counted_view += 1
     post.save()
-    context={'post':post}
-    return render(request,'test2.html',context)
-
-def test3(request,pid):
-    post = Post.objects.get(id=pid)
-    context={'post':post}
-    return render(request,'test2.html',context)
-
-    
+    context = {'post':post}
+    return render(request,'blog/blog-single.html',context)
