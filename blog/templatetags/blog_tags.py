c98fad57f31cc58291084c6d8aa6af8ef7b50af3
hesam-sa <hesam.sadat@gmail.com>
2024-07-21 15:46:29 +0330

    inclusion and filter tag practice
    practice on filter tags

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -7,3 +7,12 @@ register = template.Library()
 def function():
     posts = Post.objects.filter(status=1)
     return  posts
+
+@register.filter
+def snppets(value,arg=20):
+    return value[:arg] + '...'
+
+@register.inclusion_tag('popularpost.html')
+def popularposts():
+    posts = Post.objects.filter(status=1).order_by('-published_date')[:2]
+    return {'posts':posts}
