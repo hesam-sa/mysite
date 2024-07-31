849ad3c2889cc7fbbbe19429962eb9eb2f78d24d
hesam-sa <hesam.sadat@gmail.com>
2024-07-31 20:06:20 +0330

    authentication system
    introduce authentications

---
+++ accounts/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def login_view(request):
+    # if request.user.is_authenticated:
+    #     msg = f'user is authenticated as {request.user.username}'
+    # else:
+    #     msg = 'user is not authenticated'
+    return render(request,'accounts/login.html')#,{'msg':msg})
+
+def signup_view(request):
+    return render(request,'accounts/signup.html')
--- mysite/settings.py
+++ mysite/settings.py
@@ -49,7 +49,8 @@ INSTALLED_APPS = [
     "debug_toolbar",
     'taggit',
     'django_summernote',
-    'captcha'
+    'captcha',
+    'accounts'
 
 
     
