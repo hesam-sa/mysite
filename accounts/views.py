96db2dc7ca73a233f0c8042c84435edf2bdc35a9
hesam-sa <hesam.sadat@gmail.com>
2024-08-01 17:07:52 +0330

    login form
    download a bootstrap login page and set to login user

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login
 
 
 # Create your views here.
 def login_view(request):
-    # if request.user.is_authenticated:
-    #     msg = f'user is authenticated as {request.user.username}'
-    # else:
-    #     msg = 'user is not authenticated'
-    return render(request,'accounts/login.html')#,{'msg':msg})
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('/')
+        
+    return render(request,'accounts/login.html')
 
 def signup_view(request):
     return render(request,'accounts/signup.html')
