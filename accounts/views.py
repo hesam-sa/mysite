1c0ffab282337b85c85f6ce956c0147eceef3ae8
hesam-sa <hesam.sadat@gmail.com>
2024-08-01 18:04:00 +0330

    authentication using form
    use bult-in form for authentication and some changes

--- accounts/views.py
+++ accounts/views.py
@@ -1,18 +1,24 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+
 
 
 # Create your views here.
 def login_view(request):
     if request.method == 'POST':
-        username = request.POST["username"]
-        password = request.POST["password"]
+        form = AuthenticationForm(request=request,data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
         user = authenticate(request,username=username,password=password)
         if user is not None:
             login(request,user)
             return redirect('/')
-        
-    return render(request,'accounts/login.html')
+
+    form = AuthenticationForm()    
+    context = {'form': form}
+    return render(request,'accounts/login.html',context)
 
 def signup_view(request):
     return render(request,'accounts/signup.html')
