9cc31f5f81a641533d7d14cf8edd3dbab54951ee
hesam-sa <hesam.sadat@gmail.com>
2024-08-03 17:15:56 +0330

    login with email
    log in with email and set message for incorrect users

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.forms import AuthenticationForm,UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from accounts.forms import CustomUserCreationForm
+from django.contrib.auth.models import User
 
 
 
@@ -15,15 +16,28 @@ def login_view(request):
             if form.is_valid():
                 username = form.cleaned_data.get('username')
                 password = form.cleaned_data.get('password')
-            user = authenticate(request,username=username,password=password)
-            if user is not None:
-                login(request,user)
+            else:
+                email = form.cleaned_data.get('username')
+                form2 =User.objects.all()
+                for fr in form2:
+                    if email==fr.email:
+                        username=fr.username
+                password = form.cleaned_data.get("password")
+            try:    
+                user = authenticate(request,username=username,password=password)
+                if user is not None:
+                    login(request,user)
+                    messages.add_message(request,messages.SUCCESS,'You are Logged In')
+                    return redirect('/')
+            except UnboundLocalError:
+                messages.add_message(request,messages.ERROR,'Your username or password is Incorrect ')
                 return redirect('/')
 
         form = AuthenticationForm()    
         context = {'form': form}
         return render(request,'accounts/login.html',context)
     else:
+        
         return redirect('/')
 
 @login_required(login_url='/accounts/login')
